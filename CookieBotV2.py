c068ee4cfab7f24739db8cbf3bf59ef8957a2227
Derek <46918649+DBinnema@users.noreply.github.com>
2024-04-08 14:29:01 -0700

    Cookie count now uses cps

--- CookieBotV2.py
+++ CookieBotV2.py
@@ -1,11 +1,13 @@
 from pyautogui import *
 import pyautogui
 import time
+import datetime
 import keyboard
 import random
 import win32api, win32con
 import cv2
 import numpy as np
+import math
 
 
 def click(cookie_center_pos):
@@ -86,15 +88,24 @@ def update_Frame():
     screenImg = cv2.cvtColor(screenImg, cv2.COLOR_RGB2BGR)
     return screenImg
 
-def getBestCpS(buildinglist):
-     bestBuilding = building_list[1]
-     bestCpS = building_list[1].current_cps
+def getBestCpS(building_list):
+     bestBuilding = building_list[0]
+     bestCpS = building_list[0].current_cps
      for element in building_list:
           print('%s CpS: %s' %(element.name, element.current_cps))
           if (element.current_cps < bestCpS):
                bestBuilding=element
                bestCpS=element.current_cps
      return bestBuilding
+
+def getCurrentCpS(building_list):
+     CpS = 0
+     for element in building_list:
+
+          thisCpS = element.base_cps * element.amount
+          CpS += thisCpS
+          
+     return CpS
 
 
 
@@ -121,7 +132,7 @@ cursor_building = cv2.imread('Images/cursor_blackout.png')
 #Some info for starting logic
 cursor = Building('cursor', 0.1, 15, cursor_building)
 grandma = Building('grandma', 1, 100, grandma_building)
-print('Images loaded! /n Starting Cookie Bot')
+print('Images loaded! \n Starting Cookie Bot')
 
 
 #inital frame
@@ -138,44 +149,71 @@ frame = update_Frame()
 cursor.setClickPos(getclickposition(screenImg=frame, template=cursor_building))
 grandma.setClickPos(getclickposition(screenImg=frame, template=grandma_building))
 
-building_list = [cursor, grandma]
+active_building_list = [cursor]
+upgrade_building_list = [grandma]
+
+
 
 
 #assuming a fresh game
 cookie_count = 0
+nextPurchase = active_building_list[0]
+currentCpS = 0
 
-nextPurchase = building_list[0]
 #Bot Loop
-while keyboard.is_pressed('q') == False:      
+while keyboard.is_pressed('q') == False:
+        #To time this loop (called frame)        
+        frameStartTime = time.time()
+        
+        #Unlock the next building if we can afford it
+        if upgrade_building_list and cookie_count > upgrade_building_list[0].base_cost//2:
+             print('Unlocking %s.' %(upgrade_building_list[0].name))
+             active_building_list.append(upgrade_building_list[0])
+             upgrade_building_list = upgrade_building_list[1:]
+             
+             
+              
 
-
-        if(cookie_count > nextPurchase.current_cost):   
-                                 
+        #If the Building we want can be afforded, purchase that
+        if(cookie_count > nextPurchase.current_cost):                                
              click(nextPurchase.clickPos)
+
+             #Trying to keep cookie count relitivly accurate
              cookie_count = cookie_count - nextPurchase.current_cost
              nextPurchase.addAmount()
              print('Buying %s. Now have %s %s.' %(nextPurchase.name, nextPurchase.amount,nextPurchase.name))
-
-             nextPurchase = getBestCpS(building_list)
-
-          
-                   
+             nextPurchase = getBestCpS(active_building_list)
+             currentCpS = getCurrentCpS(active_building_list)
 
 
-
-
-          #Click the cookie
-        else:
-            click(large_cookie_center_pos)
-            cookie_count = cookie_count + 1
         
-        
+        #
+          #For the rest of the frame we click big cookie
+        frameCurrentTime = time.time()
+        frameTime = (frameCurrentTime - frameStartTime)
 
-        frameMessage = 'Estimation of cookies %s ' %cookie_count
+        while(frameTime < 0.9):
+             #For the rest of this frame, click the cookie
+             click(large_cookie_center_pos)
+             cookie_count = cookie_count + 1
+             #check the frame time aiming for 1s frames
+             frameCurrentTime = time.time()
+             frameTime = (frameCurrentTime - frameStartTime)
+        
+     
+        frameCurrentTime = time.time()
+        frameTime = (frameCurrentTime - frameStartTime)
+
+        cookies_gained_this_frame = currentCpS * frameTime
+        cookie_count += round(cookies_gained_this_frame)
+
+        ##Print a frame message for debugging
+
+        frameMessage = 'Frame took %s seconds. \nEstimated Cookies %s CpS: %s ' %(format(frameTime, "0.2f"), cookie_count, format(currentCpS, "0.2f"))
 
         print(frameMessage)
 
-        time.sleep(0.1)
+    
 
 print("Cookie Bot quitting...")
 
@@ -186,8 +224,9 @@ print("Cookie Bot quitting...")
 mapImg = cv2.circle(frame, large_cookie_center_pos, radius =5, color=(255,0,0), thickness=-7)
 mapImg = cv2.circle(frame, grandma.clickPos, radius =5, color=(255,255,0), thickness=-1)
 mapImg = cv2.circle(frame, cursor.clickPos, radius =5, color=(255,0,255), thickness=-1)
-cv2.imshow('Cookie', mapImg)
-cv2.waitKey(0)
+
+cv2.imwrite('QuitImages/GameEnd-%s.jpeg' %currentCpS, mapImg)
+
 
 
 
